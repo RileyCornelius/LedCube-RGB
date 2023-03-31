d3ac81ad78e579144a479b7b56c00c1d9e944597
Riley Cornelius <rileyccornelius@gmail.com>
2023-03-31 16:36:41 -0500

    Add SquareLine project source files

---
+++ firmware/SquareLine-Project/LedCubeDisplay/LedCubeDisplay_events.py
@@ -0,0 +1,16 @@
+
+def playButtonPressed(event_struct):
+    return
+
+
+def pauseButtonPressed(event_struct):
+    return
+
+
+def backButtonPressed(event_struct):
+    return
+
+
+def forwardButtonPressed(event_struct):
+    return
+
