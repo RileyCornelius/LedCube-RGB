1932bb36524eb91e64526f832288212354645277
Riley Cornelius <rileyccornelius@gmail.com>
2023-04-30 13:26:50 -0500

    Update ui_events with display and logging functions on T-Embed

--- firmware/T-Embed-LedCube/src/ui/ui_events.cpp
+++ firmware/T-Embed-LedCube/src/ui/ui_events.cpp
@@ -1,29 +1,33 @@
 //* This file was NOT generated
 
 #include <Arduino.h>
+#include <Logger.h>
 #include "ui.h"
-#include "../pin_config.h"
+#include "config.h"
+#include "display_com.h"
+
+static const char *TAG = "[UI Events]";
 
 void forwardButtonPressed(lv_event_t *e)
 {
-    Serial.println("Forward button pressed");
-    SerialCube.println("next");
+    LOG_DEBUG(TAG, "Forward button pressed");
+    writeDisplayCommand(CommandNext);
 }
 
 void backButtonPressed(lv_event_t *e)
 {
-    Serial.println("Back button pressed");
-    SerialCube.println("prev");
+    LOG_DEBUG(TAG, "Back button pressed");
+    writeDisplayCommand(CommandPrevious);
 }
 
 void pauseButtonPressed(lv_event_t *e)
 {
-    Serial.println("Pause button pressed");
-    SerialCube.println("pause");
+    LOG_DEBUG(TAG, "Pause button pressed");
+    writeDisplayCommand(CommandPause);
 }
 
 void playButtonPressed(lv_event_t *e)
 {
-    Serial.println("Play button pressed");
-    SerialCube.println("play");
+    LOG_DEBUG(TAG, "Play button pressed");
+    writeDisplayCommand(CommandPlay);
 }
