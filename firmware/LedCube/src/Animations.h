81cf7dbea92b8e400f1bff83e723d4f5019b011e
Riley Cornelius <54592152+RileyCornelius@users.noreply.github.com>
2023-09-06 20:40:42 -0500

    Add ghost chasing pacman

--- firmware/LedCube/src/Animations.h
+++ firmware/LedCube/src/Animations.h
@@ -412,11 +412,17 @@ public:
     Pacman()
     {
         name = __FUNCTION__;
-        setDelay(150);
+        setDelay(130);
 
         uint8_t y = 1;
 
         updateBitmap();
+    };
+
+    enum Sprite
+    {
+        PACMAN,
+        GHOST,
     };
 
     float angle = 0;
@@ -426,8 +432,12 @@ public:
     const uint16_t ghostRereshRate = 500;
     uint8_t pacmanFrame = 0;
     uint8_t ghostFrame = 0;
+    uint8_t spriteToDraw = 0;
 
     int8_t moveX = 0;
+    int8_t posY = 0;
+
+    int8_t dir = 1;
 
     void clearTempCube()
     {
@@ -465,10 +475,10 @@ public:
             }
     }
 
-    bool changeFrame()
+    bool updateSpriteFrame()
     {
         bool changed = false;
-        EVERY_N_MILLIS(70)
+        EVERY_N_MILLIS(50)
         {
             pacmanFrame++;
             if (pacmanFrame >= PACMAN_FRAME_COUNT)
@@ -476,7 +486,7 @@ public:
 
             changed = true;
         }
-        EVERY_N_MILLIS(500)
+        EVERY_N_MILLIS(250)
         {
             ghostFrame++;
             if (ghostFrame >= GHOST_FRAME_COUNT)
@@ -488,67 +498,110 @@ public:
         return changed;
     }
 
-    void rotatePacman()
+    void movePacmanGhost(uint8_t y)
     {
         Cube.clear();
-        for (int x = 0; x < CUBE_SIZE; x++)
-            for (int y = 0; y < CUBE_SIZE; y++)
-                for (int z = 0; z < CUBE_SIZE; z++)
+        for (int8_t z = 0; z < CUBE_SIZE; z++)
+            for (int8_t x = 0; x < CUBE_SIZE; x++)
+            {
+                if (spriteToDraw == PACMAN)
                 {
-                    CRGB color = tempCube[x][y][z];
-                    if (color != CRGB(0, 0, 0))
+                    uint32_t frame = pacman_data[pacmanFrame][z * CUBE_SIZE + x];
+                    if (frame >> 24 & 0xFF) // if not transparent
                     {
-                        Vector3 v = Vector3(x, y, z).rotate(angle, Axis::Z);
-                        Cube.setLed(v, color);
+                        CRGB col = CRGB((frame & 0xff), (frame >> 8) & 0xff, (frame >> 16 & 0xff)); // get rgb from data
+                        Vector3 v = Vector3(CUBE_SIZE_M1 - x, y, CUBE_SIZE_M1 - z);
+                        if (dir == 1)
+                            v = v.rotate(180, Axis::Z);
+                        v += Vector3(moveX, 0, 0);
+                        Cube.setLed(v, col);
                     }
                 }
+                else if (spriteToDraw == GHOST)
+                {
+                    uint32_t frame = ghost_data[ghostFrame][z * CUBE_SIZE + x];
+                    if (frame >> 24 & 0xFF) // if not transparent
+                    {
+                        CRGB col = CRGB((frame & 0xff), (frame >> 8) & 0xff, (frame >> 16 & 0xff)); // get rgb from data
+                        Vector3 v = Vector3(CUBE_SIZE_M1 - x, y, CUBE_SIZE_M1 - z);
+                        if (dir == 1)
+                            v = v.rotate(180, Axis::Z);
+                        v += Vector3(moveX, 0, 0);
+                        Cube.setLed(v, col);
+                    }
+                }
+            }
 
-        // Increase angle
-        angle += 10;
-        if (angle >= 360)
+        moveX = dir ? moveX + 1 : moveX - 1;
+        if (moveX < -CUBE_SIZE || moveX > CUBE_SIZE)
         {
-            angle = 0;
+            spriteToDraw = !spriteToDraw;
+            if (spriteToDraw == PACMAN)
+            {
+                dir = !dir;
+                posY = random8(0, CUBE_SIZE_M1);
+                updateTimer.setPeriod(random8(80, 150));
+            }
+            moveX = dir ? -CUBE_SIZE : CUBE_SIZE;
         }
     }
 
-    void movePacman()
-    {
-        Cube.clear();
-        for (int x = 0; x < CUBE_SIZE; x++)
-            for (int y = 0; y < CUBE_SIZE; y++)
-                for (int z = 0; z < CUBE_SIZE; z++)
-                {
-                    CRGB color = tempCube[x][y][z];
-                    if (color != CRGB(0, 0, 0))
-                    {
-                        Vector3 v = Vector3(x, y, z);
-                        // v = v.rotate(180.0, Axis::X);
-                        v += Vector3(moveX, 0, 0);
-                        Cube.setLed(v, color);
-                    }
-                }
+    // void rotatePacman()
+    // {
+    //     Cube.clear();
+    //     for (int x = 0; x < CUBE_SIZE; x++)
+    //         for (int y = 0; y < CUBE_SIZE; y++)
+    //             for (int z = 0; z < CUBE_SIZE; z++)
+    //             {
+    //                 CRGB color = tempCube[x][y][z];
+    //                 if (color != CRGB(0, 0, 0))
+    //                 {
+    //                     Vector3 v = Vector3(x, y, z).rotate(angle, Axis::Z);
+    //                     Cube.setLed(v, color);
+    //                 }
+    //             }
 
-        // // Move through the cube forward
-        // moveX++;
-        // if (moveX > CUBE_SIZE)
-        //     moveX = -CUBE_SIZE;
+    //     // Increase angle
+    //     angle += 10;
+    //     if (angle >= 360)
+    //     {
+    //         angle = 0;
+    //     }
+    // }
 
-        // Move through the cube backwards
-        moveX--;
-        if (moveX < -CUBE_SIZE)
-            moveX = CUBE_SIZE;
-    }
+    // void movePacman()
+    // {
+    //     Cube.clear();
+    //     for (int x = 0; x < CUBE_SIZE; x++)
+    //         for (int y = 0; y < CUBE_SIZE; y++)
+    //             for (int z = 0; z < CUBE_SIZE; z++)
+    //             {
+
+    //                 CRGB color = tempCube[x][y][z];
+    //                 if (color != CRGB(0, 0, 0))
+    //                 {
+    //                     Vector3 v = Vector3(x, y, z);
+    //                     // v = v.rotate(180.0, Axis::X);
+    //                     v += Vector3(moveX, 0, 0);
+    //                     Cube.setLed(v, color);
+    //                 }
+    //             }
+
+    //     // // Move through the cube forward
+    //     // moveX++;
+    //     // if (moveX > CUBE_SIZE)
+    //     //     moveX = -CUBE_SIZE;
+
+    //     // Move through the cube backwards
+    //     moveX--;
+    //     if (moveX < -CUBE_SIZE)
+    //         moveX = CUBE_SIZE;
+    // }
 
     void drawFrame() override
     {
-        // Update bitmap
-        if (changeFrame())
-        {
-            updateBitmap();
-        }
-
-        // rotatePacman();
-        movePacman();
+        updateSpriteFrame();
+        movePacmanGhost(posY);
     }
 };
 
